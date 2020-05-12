dd4f9428095fda850ecfe2cc096cd45c1551823d
Ilya Ulyanov <ilya.wt@gmail.com>
2020-05-11 23:54:00 -0700

    fix: downgrade yarn to v1 because of ncc build

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import * as core from '@actions/core'
-import * as github from '@actions/github'
 
-const run = async () => {
+async function run () {
   try {
     const token = core.getInput('repo-token', { required: true })
     console.log('token', { token })
+    const stuff = core.info('123')
   } catch (error) {
     core.error(error)
     core.setFailed(error.message)
