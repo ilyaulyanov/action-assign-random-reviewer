84e2999ea167f476f8bbcda62017b700c16b9cf1
Ilya Ulyanov <ilya.wt@gmail.com>
2020-05-11 22:28:25 -0700

    feat: initial action

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import * as core from '@actions/core'
+import * as github from '@actions/github'
+
+const run = async () => {
+  try {
+    const token = core.getInput('repo-token', { required: true })
+    console.log({ token })
+  } catch (error) {
+    core.error(error)
+    core.setFailed(error.message)
+  }
+}
+
+run()
