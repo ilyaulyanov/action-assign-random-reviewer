922718cb8b4f5eafbed752af376bff17c49a6c6b
Ilya Ulyanov <ilya.wt@gmail.com>
2020-05-13 15:14:03 -0700

    feat: get pull request context

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,25 @@
 import * as core from '@actions/core'
+import * as github from '@actions/github'
 
-async function run () {
+async function run() {
   try {
     const token = core.getInput('repo-token', { required: true })
-    console.log('token', { token })
-    const stuff = core.info('123')
+
+    const pullRequest = github.context.payload.pull_request
+    if (!pullRequest?.number) {
+      console.log('Could not get pull request number from context, exiting')
+      return
+    }
+
+    console.log({ pullRequest })
+
+    const client = new github.GitHub(token)
+    const result = await client.pulls.list({
+      owner: 'procurify',
+      repo: 'procurify-react',
+      state: 'open',
+    })
+    console.log(result)
   } catch (error) {
     core.error(error)
     core.setFailed(error.message)
