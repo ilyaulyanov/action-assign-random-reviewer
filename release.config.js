861c51d3acb6c1d37c4309feb8ca9de4e063acea
Ilya Ulyanov <ilya.wt@gmail.com>
2020-05-14 13:35:41 -0700

    docs: update changelog file

--- release.config.js
+++ release.config.js
@@ -41,7 +41,7 @@ module.exports = {
     [
 			"@semantic-release/changelog",
 			{
-				"changelogTitle": "# Changelog\n\nAll notable changes to this project will be documented in this file. See\n[Conventional Commits](https://conventionalcommits.org) for commit guidelines."
+				"changelogTitle": "# Changelog\n\nSee\n[Conventional Commits](https://conventionalcommits.org) for commit guidelines."
 			}
 		],
     [
