201930265e040bc8f78ce40fc809822de4b18cc5
Ilya Ulyanov <ilya.wt@gmail.com>
2020-05-14 13:46:19 -0700

    build: config changes [skip ci]

--- release.config.js
+++ release.config.js
@@ -3,54 +3,58 @@ module.exports = {
   debug: true,
   plugins: [
     [
-      "@semantic-release/commit-analyzer",
+      '@semantic-release/commit-analyzer',
       {
-        "releaseRules": [
+        releaseRules: [
           {
-            "type": "build",
-            "release": "patch"
+            type: 'build',
+            release: 'patch',
           },
           {
-            "type": "ci",
-            "release": "patch"
+            type: 'ci',
+            release: 'patch',
           },
           {
-            "type": "chore",
-            "release": "patch"
+            type: 'chore',
+            release: 'patch',
           },
           {
-            "type": "docs",
-            "release": "patch"
+            type: 'docs',
+            release: 'patch',
           },
           {
-            "type": "refactor",
-            "release": "patch"
+            type: 'refactor',
+            release: 'patch',
           },
           {
-            "type": "style",
-            "release": "patch"
+            type: 'style',
+            release: 'patch',
           },
           {
-            "type": "test",
-            "release": "patch"
-          }
-        ]
-      }
+            type: 'test',
+            release: 'patch',
+          },
+        ],
+      },
     ],
-    "@semantic-release/release-notes-generator",
+    '@semantic-release/release-notes-generator',
     [
-			"@semantic-release/changelog",
-			{
-				"changelogTitle": "# Changelog\n\nSee\n[Conventional Commits](https://conventionalcommits.org) for commit guidelines."
-			}
-		],
-    [
-      "@semantic-release/git",
+      '@semantic-release/changelog',
       {
-        "assets": ["package.json", "yarn.lock", "CHANGELOG.md", "dist/**/**"],
-        "message": "release(version): Release ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
-      }
+        changelogTitle:
+          '# Changelog\n\nSee\n[Conventional Commits](https://conventionalcommits.org) for commit guidelines.',
+      },
     ],
-    "@semantic-release/github"
-  ]
+    ['@semantic-release/github', {
+      assets: ['dist/**']
+    }],
+    [
+      '@semantic-release/git',
+      {
+        assets: ['package.json', 'yarn.lock', 'CHANGELOG.md'],
+        message:
+          'release(version): Release ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+  ],
 }
