5d78eba36b05a7742f43bd22ab92ae891c1b9217
Ilya Ulyanov <ilya.wt@gmail.com>
2020-05-14 13:35:06 -0700

    build: update release config [skip ci]

--- release.config.js
+++ release.config.js
@@ -47,7 +47,7 @@ module.exports = {
     [
       "@semantic-release/git",
       {
-        "assets": ["package.json", "yarn.lock", "CHANGELOG.md", "dist"],
+        "assets": ["package.json", "yarn.lock", "CHANGELOG.md", "dist/**/**"],
         "message": "release(version): Release ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
       }
     ],
